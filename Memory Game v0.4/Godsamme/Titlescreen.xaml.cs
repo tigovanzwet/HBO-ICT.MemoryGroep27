1236ec292ba6f7dcf7a22ececfa9019b09b510d7
Lukas Zijlstra <lukas.zijlstra@student.nhlstenden.com>
2020-10-22 19:06:51 +0200

    titlescreen + highscore + exit button added, werkt nu

---
+++ Memory Game v0.4/Godsamme/Titlescreen.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Godsamme
+{
+    /// <summary>
+    /// Interaction logic for Titlescreen.xaml
+    /// </summary>
+    public partial class Titlescreen : Window
+    {
+        public Titlescreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var MainWindow = new MainWindow();
+            MainWindow.Show();
+            this.Close();
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Weet u zeker dat u wilt afsluiten?");
+            this.Close();
+        }
+
+        private void highscoresBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var highscores = new highscores();
+            highscores.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Memory Game v0.4/Godsamme/highscores.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Godsamme
+{
+    /// <summary>
+    /// Interaction logic for highscores.xaml
+    /// </summary>
+    public partial class highscores : Window
+    {
+        public highscores()
+        {
+            InitializeComponent();
+        }
+
+        private void highscoresBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var Titlescreen = new Titlescreen();
+            Titlescreen.Show();
+            this.Close();
+            
+        }
+    }
+}
