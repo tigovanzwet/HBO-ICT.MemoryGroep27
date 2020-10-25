1fa825fb143c96f739e02790d0888cc67eac3afb
Tigo van Zwet <tigo2000@live.nl>
2020-10-25 17:04:34 +0100

    [MGv0.6] Kaarten worden herkend: niet/wel matchen. --> kaarten terugdraai functie

--- Memory Game v0.4/Godsamme/MainWindow.xaml.cs
+++ Memory Game v0.4/Godsamme/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,9 @@ namespace Godsamme
         private const int NR_OF_ROWS = 4;
 
         Random rnd = new Random();
+
+        Image first = null;
+        Image second = null;
 
         public MainWindow()
         {
@@ -78,18 +82,108 @@ namespace Godsamme
             var shuffled = result.OrderBy(x => Guid.NewGuid()).ToList();
             return shuffled;
 
+            
+
         }
+        
         private void TurnCard(object sender, MouseButtonEventArgs e)
         {
             Image card = (Image)sender;
             ImageSource front = (ImageSource)card.Tag;
             card.Source = front;
+            string imagename = card.Tag.ToString();
+            imagename = imagename.Substring(imagename.Length - 5);
+
+            if (card.Opacity == 0)
+            {
+                return;
+            }  
+            if (first == null)
+            {
+                first = card;
+                return;
+            }
+            if (second == null)
+            {
+                second = card;
+
+            }
+            if (first.Tag.ToString() == second.Tag.ToString())
+            {
+                MessageBox.Show("Match!");
+                first.Opacity = 0;
+                second.Opacity = 0;
+                first = null;
+                second = null;
+            }
+            else if (first.Tag.ToString() != second.Tag.ToString())
+            {
+                MessageBox.Show("No Match!");
+
+                Image backgroundImage = new Image();
+                Uri path123 = new Uri("Resources/Achterkant.png", UriKind.Relative);
+                backgroundImage.Source = new BitmapImage(path123);
+                
+                first = null;
+                second = null;
+
+                
+
+            }
+            else
+            {
+                first = null;
+                second = null;
+            }
+
+        }
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Are you sure you want to restart?");
+            var MainWindow = new MainWindow();
+            MainWindow.Show();
+            this.Close();
+
         }
 
 
+        //Tijdelijk : wordt nog verandert dus >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        private void Speler1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Speler1.Text = "";
+            Speler1.Foreground = Brushes.Black;
+
+        }
+
+        private void Speler2_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Speler2.Text = "";
+            Speler2.Foreground = Brushes.Black;
+
+        }
 
 
+        private void Speler1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Speler1.Text == "")
+            {
+                Speler1.Text = "Username player one";
+                Speler1.Foreground = Brushes.Gray;
+            }
+
+
+        }
+
+        private void Speler2_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Speler2.Text == "")
+            {
+                Speler2.Text = "Username player two";
+                Speler2.Foreground = Brushes.Gray;
+            }
+        }
     }
+    // Tijdelijk >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
 }
-
-
